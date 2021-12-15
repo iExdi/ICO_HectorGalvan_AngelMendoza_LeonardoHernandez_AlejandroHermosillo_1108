b936a5a6e715543a29308270cf4dbfb4c0e3c2a3
Alejandro Hermosillo Bermudez <91902376+AlejandroHermosilloBermudez@users.noreply.github.com>
2021-12-15 14:29:44 -0600

    Update main.c
    Se agrega la primera funcion

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 //Archivo principal
+
+//Funcion para la comparacion de dos numeros 
+float comparacion(float x, float y)
+{
+	if (x == y)
+	{
+		
+		printf("Los numeros son iguales");
+	}
+	else
+	{
+		printf("Los numeros no son iguales")
+	}
+	return 0;
+}
