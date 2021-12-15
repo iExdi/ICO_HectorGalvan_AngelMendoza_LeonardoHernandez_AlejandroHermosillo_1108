b9dfdb0cdb6a5290761ba0bf6c62ea61048e1d5d
Alejandro Hermosillo Bermudez <91902376+AlejandroHermosilloBermudez@users.noreply.github.com>
2021-12-15 14:39:22 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,1 @@
 //Archivo principal
-
-//Funcion para la comparacion de dos numeros 
-float comparacion(float x, float y)
-{
-	if (x == y)
-	{
-		
-		printf("Los numeros son iguales");
-	}
-	else
-	{
-		printf("Los numeros no son iguales")
-	}
-	return 0;
-}
