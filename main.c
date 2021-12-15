0faff8f661f69f53c80bc61a6a49984130fffbb3
Alejandro Hermosillo Bermudez <91902376+AlejandroHermosilloBermudez@users.noreply.github.com>
2021-12-15 14:27:13 -0600

    Update main.c
    Acabo de crear la primera funcion

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
-
+//Funcion de comparacion
+float comparacion(float x, float y)
+{
+	if (x == y)
+	{
+		
+		printf("Los numeros son iguales");
+	}
+	else
+	{
+		printf("Los numeros no son iguales")
+	}
+	return 0;
+}
