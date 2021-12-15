e0daf10ceec3fe5cb54ad9d264f89748321427e6
iExdi <91902347+iExdi@users.noreply.github.com>
2021-12-15 14:27:48 -0600

    main c
    Archivo principal en lenguaje C :)

--- main.c
+++ main.c
@@ -1,14 +1,1 @@
-//Funcion de comparacion
-float comparacion(float x, float y)
-{
-	if (x == y)
-	{
-		
-		printf("Los numeros son iguales");
-	}
-	else
-	{
-		printf("Los numeros no son iguales")
-	}
-	return 0;
-}
+//Archivo principal
